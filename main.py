ebcfe963d84274d37fb6a8eae9a9f99641cea26d
Claire-Backspace <Claire.Backspace@gmail.com>
2021-08-11 17:34:54 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+from requests.api import get
+from requests.sessions import session
+url = 'https://roboppn.top/'
+# r = session.get(url)
+# print(r.html.text)
+strhtml = requests.get(url)
+soupdata = BeautifulSoup(strhtml.text, 'html.parser')
+data = soupdata.select('div.col--4 > div > a > h3')
+# div.col--4:nth-child(1) > div:nth-child(2) > a:nth-child(1) > h3:nth-child(1)
+for item in data:
+   result={
+     'title':item.get_text(),
+     'link':item.get('href'),       
+     'id': re.findall('\d+',item.get('href'))
+}
