83d4cf9650ad9b268b5a4c81e9af3b06d42a2540
Claire <77777125+Claire-Backspace@users.noreply.github.com>
2021-08-11 23:57:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
+from typing import Text
 import requests
-import re
 from bs4 import BeautifulSoup
-from requests.api import get
-from requests.sessions import session
-url = 'https://roboppn.top/'
-# r = session.get(url)
-# print(r.html.text)
-strhtml = requests.get(url)
-soupdata = BeautifulSoup(strhtml.text, 'html.parser')
-data = soupdata.select('div.col--4 > div > a > h3')
-# div.col--4:nth-child(1) > div:nth-child(2) > a:nth-child(1) > h3:nth-child(1)
-for item in data:
-   result={
-     'title':item.get_text(),
-     'link':item.get('href'),       
-     'id': re.findall('\d+',str( item.get('href') ))
-}
+
+r = requests.get('https://roboppn.top/docs/README')
+source = r.content
+soup = BeautifulSoup(source, 'lxml')
+
+#print(soup.prettify())
+#print(soup.find('div'))
+soup_new = soup.find('div', id = ("__docusaurus"))
+
+for chapters in  soup_new.find_all('a'):
+
+         print(chapters.text)
+         #print(f"https://{chapters.attrs['href']}")
+         print(chapters.attrs['href'])
